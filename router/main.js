6bd4623a1a87bd5d58ccf523525010ae71b1daa3
최윤주 <yeomju311@gmail.com>
2019-12-16 23:50:52 +0900

    [기타] 폴더 정리

--- router/main.js
+++ router/main.js
@@ -2,46 +2,46 @@ module.exports = function(app)
 {
     // 메인페이지
      app.get('/',function(req,res){
-        res.render('index.html')
+        res.render('main.html')
      });
 
      //로그인 페이지
      app.get('/login',function(req,res){
-        res.render('index.html')
+        res.render('login.html')
      });
 
      // 회원가입 페이지
      app.get('/register',function(req,res){
-        res.render('index.html')
+        res.render('register.html')
      });
 
      // about 페이지
-     app.get('/register',function(req,res){
-        res.render('index.html')
+     app.get('/about',function(req,res){
+        //res.render('index.html')
      });
 
      // 미션지 검색 페이지
      app.get('/mission_link',function(req,res){
-        res.render('index.html')
+        res.render('missionform_search.html')
      });
 
      // 미션 결과 페이지
      app.get('/mission_result',function(req,res){
-        res.render('index.html')
+        //res.render('.html')
      });
 
      // 미션 정보 페이지
      app.get('/mission_info',function(req,res){
-        res.render('index.html')
+        res.render('missiontx_search.html')
      });
 
      // 마이페이지 - 내 정보
      app.get('/mypage',function(req,res){
-        res.render('index.html')
+        res.render('mypage.html')
      });
 
      // 마이페이지 - 내 기록
      app.get('/mypage/record',function(req,res){
-        res.render('index.html')
+        //res.render('index.html')
      });
 }
