0f6c7e5090b0f2826846c4be7c906fdf4a4b63dd
최윤주 <yeomju311@gmail.com>
2019-12-19 11:00:12 +0900

    [기능] 아이디, 닉네임 중복 체크

--- router/config.js
+++ router/config.js
@@ -2,30 +2,29 @@ var mysql = require('mysql');
 
 // 커넥션을 정의합니다.
 // RDS Console 에서 본인이 설정한 값을 입력해주세요.
-var db = mysql.createConnection({
-    host: "",
-    user: "admin",
-    password: "",
-    database: "promission",
-    port:"3306"
-  });
-  //let db = require('./login')(connection);
-  
-  // RDS에 접속합니다.
-  db.connect(function(err) {
-    if (err) {
-      throw err; // 접속에 실패하면 에러를 throw 합니다.
-    } else {
-      // 접속
-      console.log("데이터베이스 접속 완료");
-      //db.query('insert into user_info(user_id, nickname, password, email, wallet_address, link_address) values ("gggg", "gggggdfd", "sjdksdglsdf", "y@dd.com", "sdjkfldfkdl", "dlfkdfkldjk");');
+var connection = mysql.createConnection({
+  host: "",
+  user: "admin",
+  password: "",
+  database: "promission",
+  port: "3306"
+});
+//let db = require('./login')(connection);
 
-      /*
-      connection.query("SELECT * FROM fruit", function(err, rows, fields) {
-        console.log(rows); // 결과를 출력합니다!
-      });
-      */
-    }
-  });
+// RDS에 접속합니다.
+connection.connect(function (err) {
+  if (err) {
+    throw err; // 접속에 실패하면 에러를 throw 합니다.
+  } else {
+    // 접속
+    console.log("데이터베이스 접속 완료");
 
-  module.exports.db = db; 
+    /*
+    connection.query("SELECT * FROM fruit", function(err, rows, fields) {
+      console.log(rows); // 결과를 출력합니다!
+    });
+    */
+  }
+});
+
+module.exports.connetion = connection; 
--- router/login.js
+++ router/login.js
@@ -1,8 +1,71 @@
 let db = require('./config')
 
+let debug = true;
+
 module.exports = function (app) {
-    app.get('/', function (req, res){
+    app.get('/', function (req, res) {
         console.log('message ');
+    });
+
+    // id 중복 확인
+    app.post("/user/check/id", function (req, res) {
+        let m_user_id = req.body.user_id;
+
+        db.connetion.query('select user_id from user_info where user_id="' + m_user_id + '";', function (err, rows, fields) {
+            if (debug) {
+                console.log('rows :', rows);
+                console.log('fields :', fields);
+            }
+
+            if (err) {
+                res.send({
+                    result_code: 500,
+                    message: "에러"
+                });
+            }
+            else {
+                if (rows[0]) {
+                    res.send({
+                        result_code: 503,
+                        message: "중복된 아이디 존재"
+                    });
+                }
+                else {
+                    res.send({
+                        result_code: 200,
+                        message: "가입 가능한 아이디"
+                    });
+                }
+            }
+        });
+    });
+
+    // 닉네임 중복 확인
+    app.post("/user/check/nickname", function (req, res) {
+        let m_nickname = req.body.nickname;
+
+        db.connetion.query('select nickname from user_info where nickname="' + m_nickname + '";', function (err, rows, fields) {
+            if (err) {
+                res.send({
+                    result_code: 500,
+                    message: "에러"
+                });
+            }
+            else {
+                if (rows[0]) {
+                    res.send({
+                        result_code: 503,
+                        message: "중복된 닉네임 존재"
+                    });
+                }
+                else {
+                    res.send({
+                        result_code: 200,
+                        message: "가입 가능한 닉네임"
+                    });
+                }
+            }
+        });
     });
 
     // 회원가입
@@ -14,17 +77,19 @@ module.exports = function (app) {
         let m_email = req.body.email;
         let m_game_id = req.body.game_id;
 
-        console.log("user_id", m_user_id);
-        console.log("m_nickname", m_nickname);
-        console.log("m_password", m_password);
-        console.log("m_wallet_address", m_wallet_address);
-        console.log("m_email", m_email);
-        console.log("m_game_id", m_game_id);
-
+        if (debug) {
+            console.log("user_id", m_user_id);
+            console.log("m_nickname", m_nickname);
+            console.log("m_password", m_password);
+            console.log("m_wallet_address", m_wallet_address);
+            console.log("m_email", m_email);
+            console.log("m_game_id", m_game_id);
+        }
 
         // DB에 저장
-        db.db.query('INSERT INTO USER_INFO(user_id, nickname, password, wallet_address, email) VALUES ("' + m_user_id + '","' + m_nickname + '","' + m_password + '","' + m_wallet_address + '","' + m_email + '")', function(err, rows, fields){
-            if(err) {
+        db.connetion.query('insert into user_info(user_id, nickname, password, wallet_address, email) values ("' + m_user_id + '","' + m_nickname + '","' + m_password + '","' + m_wallet_address + '","' + m_email + '");', function (err, rows, fields) {
+            if (err) {
+                console.log(err);
                 res.send({
                     result_code: 503,
                     message: '회원가입 실패'
@@ -36,7 +101,7 @@ module.exports = function (app) {
                     message: '회원가입 성공'
                 });
             }
-        })
+        });
     });
 
     // 로그인
@@ -45,7 +110,7 @@ module.exports = function (app) {
         let m_password = req.body.password;
 
         // DB에 저장된 내용과 비교
-
+        
 
         res.send({
             result_code: 200,
@@ -73,7 +138,7 @@ module.exports = function (app) {
     });
 
     // 비밀번호 변경 (로그인 후)
-    app.put("/user/password/change", function (req, res){
+    app.put("/user/password/change", function (req, res) {
 
     });
 
