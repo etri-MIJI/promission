62ca4ee5e04abb9be21737cf1dd026de91fcba08
최윤주 <yeomju311@gmail.com>
2019-12-27 02:27:52 +0900

    [버그] 데이터 접근 수정

--- public/js/missionresult.js
+++ public/js/missionresult.js
@@ -5,10 +5,10 @@ if (DEBUG) console.log('missionresult.js');
 $(document).ready(function() {
   //DB에서 전체 리스트  get
   $.get('/result-page/list', function(data) {
-    let m = data[0].title;
-    console.log('전체 data[0].title : ', m);
+    //let m = data[0].title;
+    //console.log('전체 data[0].title : ', m);
     let table = document.getElementById('missionTable');
-    let length = data.length;
+    let length = data.list.length;
     //clearTable();
     console.log('전체 data : ', data);
     for (let i = 0; i < length; i++) {
@@ -21,11 +21,11 @@ $(document).ready(function() {
       let cell6 = row.insertCell(5);
       // let cell4 = row.insertCell(3);
       cell1.innerHTML = i + 1;
-      cell2.innerHTML = data[i].nickname;
-      cell3.innerHTML = data[i].mission_tx;
-      cell4.innerHTML = data[i].start_time;
-      cell5.innerHTML = data[i].end_time;
-      cell6.innerHTML = data[i].result;
+      cell2.innerHTML = data.list[i].nickname;
+      cell3.innerHTML = data.list[i].mission_tx;
+      cell4.innerHTML = data.list[i].start_time;
+      cell5.innerHTML = data.list[i].end_time;
+      cell6.innerHTML = data.list[i].result;
     }
   }); // end of $.get('/result-page/list', function(data)
 
@@ -37,10 +37,10 @@ $(document).ready(function() {
 
     //DB에 닉네임 서치해서 get
     $.get('/result-page/search/' + id, function(data) {
-      let m = data[0].title;
-      console.log('서치 data[0].title : ', m);
+      //let m = data[0].title;
+      //console.log('서치 data[0].title : ', m);
       let table = document.getElementById('missionTable');
-      let length = data.length;
+      let length = data.list.length;
       //clearTable();
       console.log('서치 data : ', data);
       for (let i = 0; i < length; i++) {
@@ -53,11 +53,11 @@ $(document).ready(function() {
         let cell6 = row.insertCell(5);
         // let cell4 = row.insertCell(3);
         cell1.innerHTML = i + 1;
-        cell2.innerHTML = data[i].nickname;
-        cell3.innerHTML = data[i].mission_tx;
-        cell4.innerHTML = data[i].start_time;
-        cell5.innerHTML = data[i].end_time;
-        cell6.innerHTML = data[i].result;
+        cell2.innerHTML = data.list[i].nickname;
+        cell3.innerHTML = data.list[i].mission_tx;
+        cell4.innerHTML = data.list[i].start_time;
+        cell5.innerHTML = data.list[i].end_time;
+        cell6.innerHTML = data.list[i].result;
       }
     }); // end of get
   });
