ea8e1e1cbaea9c37e6f756537350d24ce2e5921a
최윤주 <yeomju311@gmail.com>
2019-12-26 15:20:24 +0900

    [기능] 미션폼 제출 시 정보 저장

--- router/mission_link.js
+++ router/mission_link.js
@@ -116,4 +116,28 @@ module.exports = function (app) {
         });
         
     });
+
+    // 미션폼 제출시 정보를 저장
+    app.post('/submit/mission', function (req, res){
+        m_user_id = req.body.user_id;
+        m_mission_tx = req.body.mission_tx;
+        m_mission_type = req.body.mission_type;
+        var datetime = new Date();
+        db.connetion.query('insert into mission_info (user_id, mission_tx, mission_type, mission_time) values(?, ?, ?, ?);',[m_user_id, m_mission_tx, m_mission_type, datetime], function(err, rows, fields){
+            if(err){
+                console.log('/submit/mission err: ',err);
+                res.send({
+                    result_code: 500,
+                    message: '에러'
+                });
+            }
+            else {
+                console.log('/submic/mission rows: ', rows);
+                res.send({
+                    result_code: 200,
+                    message: '성공'
+                })
+            }
+        });
+    });
 }
