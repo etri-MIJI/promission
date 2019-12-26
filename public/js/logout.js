8b72312d76d24735eb0bb5505ec4e5eb308059e2
boogi0472 <56065136+boogi0472@users.noreply.github.com>
2019-12-26 23:23:37 +0900

    [버그] DB 요청 타입 변경

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,7 +3,7 @@ console.log('logout.js');
 $(document).ready(function() {
   $('#logout_button').on('click', function() {
     //DB에 post
-    $.post('/user/logout', {}, function(data) {
+    $.delete('/user/logout', {}, function(data) {
       console.log('logout_button : ', data);
     }); // end of post
   }); //end of button
