d03373de917e546e74411739a1a8694f85f448ba
Yetaeng <56065130+Yetaeng@users.noreply.github.com>
2019-12-29 20:40:46 +0900

    [기능] 미션저장 및 호출 컨트랙트 ABI주소

---
+++ contracts/MissionABI.js
@@ -0,0 +1,156 @@
+const ContractABI = [
+  {
+    constant: false,
+    inputs: [],
+    name: 'deleteContract',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_address',
+        type: 'address',
+      },
+      {
+        name: '_BJaddress',
+        type: 'address',
+      },
+      {
+        name: '_Kill',
+        type: 'uint256',
+      },
+      {
+        name: '_killAmount',
+        type: 'uint256',
+      },
+    ],
+    name: 'setInstructorKill',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_address',
+        type: 'address',
+      },
+      {
+        name: '_BJaddress',
+        type: 'address',
+      },
+      {
+        name: '_rank',
+        type: 'uint256',
+      },
+      {
+        name: '_rankAmount',
+        type: 'uint256',
+      },
+    ],
+    name: 'setInstructorRank',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_address',
+        type: 'address',
+      },
+    ],
+    name: 'getInstructorKill',
+    outputs: [
+      {
+        name: '',
+        type: 'address',
+      },
+      {
+        name: '',
+        type: 'uint256',
+      },
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_address',
+        type: 'address',
+      },
+    ],
+    name: 'getInstructorRank',
+    outputs: [
+      {
+        name: '',
+        type: 'address',
+      },
+      {
+        name: '',
+        type: 'uint256',
+      },
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getInstructors',
+    outputs: [
+      {
+        name: '',
+        type: 'address[]',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'instructorAccts',
+    outputs: [
+      {
+        name: '',
+        type: 'address',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
