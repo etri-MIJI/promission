ee6ef68a3f2b48d42f36bfe2e4c8c587785bcb9d
boogi0472 <56065136+boogi0472@users.noreply.github.com>
2019-12-29 18:22:10 +0900

    [버그] 테이블 버그 수정

--- public/js/missionresult.js
+++ public/js/missionresult.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
     //let m = data[0].title;
     //console.log('전체 data[0].title : ', m);
     let table = document.getElementById('missionTable');
+    table.innerHTML =
+      '<tr><td>순번</td><td>닉네임</td><td>미션 트랜잭션 주소</td><td>미션 시작 시간</td><td>미션 종료 시간</td><td>미션 결과</td></tr>';
     let length = data.list.length;
     //clearTable();
     console.log('전체 data : ', data);
@@ -25,21 +27,27 @@ $(document).ready(function() {
       cell3.innerHTML = data.list[i].mission_tx;
       cell4.innerHTML = data.list[i].start_time;
       cell5.innerHTML = data.list[i].end_time;
-      cell6.innerHTML = data.list[i].result;
+      if (data.list[i].result == 1) {
+        cell6.innerHTML = '성공';
+      } else {
+        cell6.innerHTML = '실패';
+      }
     }
   }); // end of $.get('/result-page/list', function(data)
 
   //검색 버튼 클릭
-  $('#nick_search').on('click', function() {
-    let id = $('#nickname').val();
+  $('#nickname').on('click', function() {
+    let nick = $('#mission_search').val();
 
-    if (DEBUG) console.log('id:', id);
+    if (DEBUG) console.log('nick:', nick);
 
     //DB에 닉네임 서치해서 get
-    $.get('/result-page/search/' + id, function(data) {
+    $.get('/result-page/search/' + nick, function(data) {
       //let m = data[0].title;
       //console.log('서치 data[0].title : ', m);
       let table = document.getElementById('missionTable');
+      table.innerHTML =
+        '<tr><td>순번</td><td>닉네임</td><td>미션 트랜잭션 주소</td><td>미션 시작 시간</td><td>미션 종료 시간</td><td>미션 결과</td></tr>';
       let length = data.list.length;
       //clearTable();
       console.log('서치 data : ', data);
@@ -57,8 +65,12 @@ $(document).ready(function() {
         cell3.innerHTML = data.list[i].mission_tx;
         cell4.innerHTML = data.list[i].start_time;
         cell5.innerHTML = data.list[i].end_time;
-        cell6.innerHTML = data.list[i].result;
+        if (data.list[i].result == 1) {
+          cell6.innerHTML = '성공';
+        } else {
+          cell6.innerHTML = '실패';
+        }
       }
     }); // end of get
-  });
+  }); //end of nick_search click
 });
